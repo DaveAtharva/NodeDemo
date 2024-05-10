204b4de81b3db4b7afc8659a482bbb6897aa1d25
Atharva Dave <atharvadave28@gmail.com>
2024-05-10 18:21:42 +0530

    Final Version

--- db.js
+++ db.js
@@ -1,7 +1,9 @@
 // This FIle is responsible for the connection with MONGODB
 const mongoose = require('mongoose');
+require('dotenv').config(); 
 // const mongodburl = 'mongodb://localhost:27017/Demo'; //(Demo)-> Name of Database
-const mongodburl = 'mongodb+srv://Demo:12345@demo.zgajllv.mongodb.net/';
+const mongodburl = process.env.DB_URL;
+
 // Estalishing Connection
 mongoose.connect(mongodburl);
 
